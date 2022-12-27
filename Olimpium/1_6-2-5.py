37ca43fa7378280946e512dfc3d8d684c082c5db
YuriyNikolaev <raptor1980@mail.ru>
2022-12-27 12:09:16 +1000

    подправил дз в 1-6-2

--- Olimpium/1_6-2-5.py
+++ Olimpium/1_6-2-5.py
@@ -1,9 +1,22 @@
-x = int(input())
-p = int(input())
-y = int(input())
+# 1_6-2-5.py
+
+# x = int(input())
+# p = int(input())
+# y = int(input())
+# i = 0
+# while x < y:
+#     x *= 1 + p / 100
+#     x = int(100 * x) / 100
+#     i += 1
+# print(i)
+
+#мой вариант решения
+x = 1000 # начальный вклад
+p = 0.1 # проценты
+y = 5000 # конечная цель вклада
 i = 0
 while x < y:
-    x *= 1 + p / 100
-    x = int(100 * x) / 100
+    x = int(x + p * x);
     i += 1
-print(i)
+    print(x)
+    print(i)
