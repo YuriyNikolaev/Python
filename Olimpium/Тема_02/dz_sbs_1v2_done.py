ed262165db4f9f96eb9f1aee7ef98400da3d2221
YuriyNikolaev <raptor1980@mail.ru>
2023-01-18 21:20:04 +1000

    игра к-н-б до варианта №3

---
+++ Olimpium/Тема_02/dz_sbs_1v2_done.py
@@ -0,0 +1,43 @@
+# dz_sbs_2.py -- камень ножницы бумага
+from random import *
+
+# выбор игрока
+# камень = 1, ножницы = 2, бумага = 3
+user = int(input("Введите 1 - камень, 2 - ножницы, 3 - бумага"))
+
+# выбор компьютера
+pc = randrange(1, 3, 1)
+
+# кол-во побед
+win_user = 0
+win_pc = 0
+
+
+# игровой цикл
+while user != 0:
+	# проверка условий
+	
+    if user == pc:
+        print("Ничья!")
+    elif user == 1 and pc == 2:
+        print("Выиграл игрок")
+        win_user += 1
+    elif user == 2 and pc == 3:
+        print("Выиграл игрок")
+        win_user += 1
+    elif user == 3 and pc == 1:
+        print("Выиграл игрок")
+        win_user += 1
+    else:
+        print("Выиграл комп")
+        win_pc += 1
+
+	# выбор компьютера
+    pc = randrange(1, 4, 1)
+	
+	# выбор игрока
+    user = int(input("Введите 1 - камень, 2 - ножницы, 3 - бумага, 0 - выход"))
+
+print("win_user = ", win_user)
+print("win_pc = ", win_pc)
+print("До свидания!")
