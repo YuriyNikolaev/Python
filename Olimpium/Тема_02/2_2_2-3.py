11623d88c0091aa2313583e9f8c14393763d6ba7
YuriyNikolaev <raptor1980@mail.ru>
2023-01-26 11:24:19 +1000

    модуль 02, добавил решения задач

---
+++ Olimpium/Тема_02/2_2_2-3.py
@@ -0,0 +1,12 @@
+# 2_2_2-3.py
+
+# num = input("Введите телефонный номер в формате х(ххх)ххххххх")
+num = "8(914)2323232"
+simbol = ['(', ')']
+
+for elem in simbol:
+    num = (num.replace(elem,""))
+    if num.isdigit() and len(num) == 11:
+        print(f"{num}", end='\n\n')
+    else:
+        print(f"Вы ввели не правильно тлф номер\n", end='\n\n')
