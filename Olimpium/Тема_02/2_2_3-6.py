b75956b9c18b0abc621abc8a0796744cf64d4ee7
YuriyNikolaev <raptor1980@mail.ru>
2023-02-27 12:27:50 +1000

    Модуль 02. Тема 2. Решение задач

--- Olimpium/Тема_02/2_2_3-6.py
+++ Olimpium/Тема_02/2_2_3-6.py
@@ -1,4 +1,5 @@
 # 2_2_3-6.py
+# Тема 2. Занятие 3. Этап 2. Задача 6
 
 # Удалить из строки все символы, заключенные в скобки.
 
@@ -6,21 +7,15 @@
 
 my_string = "abcd(123)efg"
 new_string = " "
-srez = my_string["(":")"]
 
 for i in my_string:
-    print(i, end=",")
-    if i in srez:
+    # print(i, end=",")
+    if i == '(' or i == ')':
         continue
     else:
         new_string += i
 
 print(new_string)
-    
-        
-
-# print(my_string)
-
 
  
 
