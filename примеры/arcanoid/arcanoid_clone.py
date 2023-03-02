c17850dfb27410ac6bc43997d59cd188ca353d4d
YuriyNikolaev <raptor1980@mail.ru>
2023-03-03 00:00:27 +1000

    обновил картинки, внес новый код

--- примеры/arcanoid/arcanoid_clone.py
+++ примеры/arcanoid/arcanoid_clone.py
@@ -8,7 +8,7 @@ TITLE = "Arkanoid Clone"
 WIDTH = 800
 HEIGHT = 500
 
-paddle = Actor("paddleblue.png")
+paddle = Actor("paddleblu.png")
 paddle.x = 120
 paddle.y = 420
 
@@ -20,12 +20,17 @@ bar = Actor("element_blue_rectangle_glossy.png")
 bar.x = 120
 bar.y = 100
 
+bars_list = []
+
 
 def draw():
     screen.blit("background.png", (0, 0))
     bar.draw()
     paddle.draw()
     ball.draw()
+    place_blue_bars()
+
+def place_blue_bars():
     bar_x = 120
     bar_y = 100
     for i in range(8):
@@ -36,9 +41,29 @@ def draw():
 
         bar_x += 70
 
+def place_bars(x, y, image):
+    bar_x = x
+    bar_y = y
+    for i in range(8):
+        bar = Actor(image)
+        bar.x = bar_x
+        bar.y = bar_y
+        bar_x += 70
+        bars_list.append(bar)
+
+
 
 def update():
     pass
 
+
+coloured_box_list = ["element_blue_rectangle_glossy.png", 
+                    "element_green_rectangle_glossy.png", 
+                    "element_red_rectangle_glossy.png"]
+x = 120
+y = 100
+for coloured_box in coloured_box_list:
+    place_bars(x, y, coloured_box)
+    y += 50
 
 pgzrun.go()
