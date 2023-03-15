6db35d6b19b899800c9e42a1eb5e090d0cb2c9d7
YuriyNikolaev <raptor1980@mail.ru>
2023-03-15 22:02:39 +1000

    Новый мини-проект 04_blackjack.py

---
+++ примеры/BigBookSmallPrjts/04_blackjack.py
@@ -0,0 +1,26 @@
+# 04_blackjack.py
+"""Blackjack, by Al Sweigart al@inventwithpython.com
+2. The classic card game also known as 21. (This version doesn't have
+3. splitting or insurance.)
+4. More info at: https://en.wikipedia.org/wiki/Blackjack
+5. View this code at https://nostarch.com/big-book-small-python-projects
+6. Tags: large, game, card game"""
+
+import random, sys
+
+# Set up the constants:
+HEARTS = chr(9829)   # Character 9829 is '♥'.
+DIAMONDS = chr(9830) # Character 9830 is '♦'.
+SPADES = chr(9824)   # Character 9824 is '♠'.
+CLUBS = chr(9827)    # Character 9827 is '♣'.
+# (A list of chr codes is at https://inventwithpython.com/charactermap)
+BACKDISE = 'backside'
+
+def main():
+    pass
+
+
+
+# If the program is run (instead of imported), run the game:
+if __name__ =='__main__':
+    main()
