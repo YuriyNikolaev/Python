7d5a255f8f6de7eb6a5b633bd7f379a64d53169a
YuriyNikolaev <raptor1980@mail.ru>
2023-03-15 22:37:51 +1000

    набор кода игры Блэкджек

--- примеры/BigBookSmallPrjts/04_blackjack.py
+++ примеры/BigBookSmallPrjts/04_blackjack.py
@@ -17,7 +17,46 @@ CLUBS = chr(9827)    # Character 9827 is '♣'.
 BACKDISE = 'backside'
 
 def main():
-    pass
+    print('''Blackjack, by Al Sweigart al@inventwithpython.com
+
+    Rules:
+        Try to get as close to 21 without going over.
+        Kings, Queens, and Jacks are worth 10 points.
+        Aces are worth 1 or 11 points.
+        Cards 2 through 10 are worth their face value.
+        (H)it to take another card.
+        (S)tand to stop taking cards.
+        On your first play, you can (D)ouble down to increase your bet
+        but must hit exactly one more time before standing.
+        In case of a tie, the bet is returned to the player.
+        The dealer stops hitting at 17.''')
+
+    money = 5000
+    while True: # Main game loop.
+        # Check if the player has run out of money:
+        if money <= 0:
+            print("You're broke!")
+            print("Good thing you weren't playing with real money.")
+            sys.exit()
+        
+        # Let the player enter their bet for this round:
+        print('Money:', money)
+        bet = getBet(money)
+
+        # Give the dealer and player two cards from the deck each:
+        deck = getDeck()
+        dealerHand = [deck.pop(), deck.pop()]
+        playerHand = [deck.pop(), deck.pop()]
+
+        # Yandle player actions:
+        print('Bet:', bet)
+        while True: # Keep looping until players stands or busts.
+            displayHands(playerHand, dealerHand, False)
+            print()
+
+            # Check if the player has bust:
+            if getHandValue(playerHand) > 21:
+                break
 
 
 
