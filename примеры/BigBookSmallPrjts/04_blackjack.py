12241e26ff8132ee8c5159544ca453742e61db76
YuriyNikolaev <raptor1980@mail.ru>
2023-03-20 17:01:36 +1000

    допечатал код игры 04_blackjack.py

--- примеры/BigBookSmallPrjts/04_blackjack.py
+++ примеры/BigBookSmallPrjts/04_blackjack.py
@@ -43,14 +43,14 @@ def main():
 
         # Let the player enter their bet for this round:
         print('Money:', money)
-        bet = getBet(money)
+        bet = getBet(money)  # bet - ставка
 
         # Give the dealer and player two cards from the deck each:
         deck = getDeck()
         dealerHand = [deck.pop(), deck.pop()]
         playerHand = [deck.pop(), deck.pop()]
 
-        # Yandle player actions:
+        # Handle player actions:
         print('Bet:', bet)
         while True:  # Keep looping until players stands or busts.
             displayHands(playerHand, dealerHand, False)
@@ -78,9 +78,48 @@ def main():
                 print('You drew a {} of {}'.format(rank, suit))
                 playerHand.append(newCard)
 
-                if getHandValue(playHand) > 21:
+                if getHandValue(playerHand) > 21:
                     # The player has busted:
                     continue
+
+            if move in ('S', 'D'):
+                # Stand|doubling down stops the player's turn.
+                break
+
+        # Handle the dealer's actions:
+        if getHandValue(playerHand) <= 21:
+            while getHandValue(dealerHand) < 17:
+                # The dealer hits:
+                print('Dealer hits...')
+                dealerHand.append(deck.pop())
+                displayHands(playerHand, dealerHand, False)
+
+                if getHandValue(dealerHand) > 21:
+                    break # The dealer has busted.
+                input('Press Enter to continue...')
+                print('\n\n')
+
+        # Show the final hands:
+        displayHands(playerHand, dealerHand, True)
+
+        playerValue = getHandValue(playerHand)
+        dealerValue = getHandValue(dealerHand)
+        # Handle wither the player won, lost, or tied:
+        if gealerValue > 21:
+            print('Dealer busts! You win ${}!'.format(bet))
+            money += bet
+        elif (playerValue > 21) or (playerValue < dealerValue):
+            print('You lost!')
+            money -= bet
+        elif playerValue > dealerValue:
+            print('You won ${}!'.format(bet))
+            money += bet
+        elif playerValue == dealerValue:
+            print('It\'s a tie, the bet is returned to you.')
+
+        input('Press Enter to continue...')
+        print('\n\n')
+
 
 
 def getBet(maxBet):
@@ -105,6 +144,79 @@ def getDeck():
     for suit in (HEARTS, DIAMONDS, SPADES, CLUBS):
         for rank in range(2, 11):
             deck.append((str(rank), suit)) # Add the number cards.
+        for rank in ('J', 'Q', 'K', 'A'):
+            deck.append((rank, suit))      # Add the face and ace cards.
+    random.shuffle(deck)
+    return deck
+
+def displayHands(playerHand, dealerHand, showDealerHand):
+    """Show the player's and dealer's cards/ Hide the dealer's first
+    card if showDealerHande False. """
+    print()
+    if showDealerHand:
+        print('DEALER: ', getHandValue(dealerHand))
+        displayCards(dealerHand)
+    else:
+        print('DEALER: ???')
+        # Hide the dealer's first card:
+        displayCards([BACKDISE] + dealerHand[1:])
+
+    # Show the player's cards:
+    print('PLAYER: ', getHandValue(playerHand))
+    displayCards(playerHand)
+
+def getHandValue(cards):
+    """Returns the value of the cards. Face cards are worth 10, 
+    aces are worth 11 or 1 (this function picks the most 
+    suitable ace value). """
+    value = 0
+    numberOfAce = 0
+
+    # Add tha value for the non-ace cards:
+    for card in cards:
+        rank = card[0] # card is a tuple like (rank, suit)
+        if rank == 'A':
+            numberOfAce += 1
+        elif rank in ('K', 'Q', 'J'):  # Face cards are worth 10 points.
+            value += 10
+        else:
+            value += int(rank) # Number cards are worth their number.
+
+    # Add the value for the aces:
+    value += numberOfAce # Add 1 per ace.
+    for i in range(numberOfAce):
+        # If another 10 can be added with busting, do so:
+        if value + 10 <= 21:
+            value += 10
+
+    return value
+
+    
+def displayCards(cards):
+    """Display all the cards in the cards list. """
+    rows = ['', '', '', '', ''] # The text to display on each row.
+
+    for i, card in enumerate(cards):
+        rows[0] += ' ___  ' # Print the top line of the card.
+        if card == BACKDISE:
+            # Print a card's back:
+            rows[1] += '|## | '
+            rows[2] += '|###| '
+            rows[3] += '|_##| '
+        else:
+            # Print the card;s front:
+            rank, suit = card # The card is a tuple data structure.
+            rows[1] += '|{} | '.format(rank.ljust(2))
+            rows[2] += '| {} | '.format(suit)
+            rows[3] += '|_{}| '.format(rank.ljust(2, '_'))
+
+    #Print each row on the screen:
+    for row in rows:
+        print(row)
+
+def getMove(playerHand, money):
+    pass
+
 
 # If the program is run (instead of imported), run the game:
 if __name__ == '__main__':
